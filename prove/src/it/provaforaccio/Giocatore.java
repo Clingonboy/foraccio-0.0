0238664167357ce713e0d380b9e7da7981e12788
eugenio <eugenio@eugenio-pc>
2017-01-30 06:11:18 +0100

    Piccole revisioni

--- prove/src/it/provaforaccio/Giocatore.java
+++ prove/src/it/provaforaccio/Giocatore.java
@@ -1,26 +1,43 @@
 package it.provaforaccio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Giocatore {
-
-	private String nome;
-	private int posGiocatore;
-	private List<Carta> carteInMano;
-	private List<Carta> carteGiocate;
+	
+	private String nome; // Nome del giocatore		
+	private int posGiocatore; // Posizione sul tavolo
+	private List<Carta> carteInMano; // ArrayList con le carte in mano del giocaotre
+	private List<Carta> carteGiocate; // ArreyList con le carte giocate
+	private int manoInCorso;
+	
 	
 	
 	/**
 	 * Costrutotre base
 	 */
 	public Giocatore(){
-		
+		carteInMano = new ArrayList<Carta>();
+		carteGiocate = new ArrayList<Carta>();
+		manoInCorso = 0;
 	}
 	
+	/**
+	 * Importa una lista di carte e le fa diventare <b>{@code carteInMano}<b>
+	 * @param carteRicevute {@code ArrayList<Carta>}
+	 */
 	public void setCarteInMano(List<Carta> carteRicevute)
 	{
 		carteInMano = carteRicevute;
 	}
 	
-	
+	/**
+	 * Metodo privato che aggiunge alla lista di carte giocate
+	 * l'ultama carta giocata
+	 * @param cartaGiocata
+	 */
+	private void addCartaGiocata(Carta cartaGiocata)
+	{
+		carteGiocate.add(cartaGiocata);
+	}
 }
